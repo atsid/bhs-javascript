9d62374570756637ea1aadc0abe9c22ac730dace
talkswithnumbers <github@talkswithnumbers.com>
2014-04-24 09:23:35 -0700

    swapped variable and function script code

--- 01.Variables/script.js
+++ 01.Variables/script.js
@@ -1,15 +1,11 @@
-//adds two items together.
-//if the items are numbers, it will be a sum
-//if the items are strings, they will be stitched together
-function add(x, y) {
-    return x + y;
-}
-
-
-var z = add(1, 3);
+var x = 1;
+var y = 2;
+var z = x + y;
 
 print("z is " + z);
 
-var fullName = add("Bremerton ", "High");
+var firstName = "Bremerton";
+var lastName = "High";
+var fullName = firstName + " " + lastName;
 
 print("fullName is " + fullName);
--- 02.Functions/script.js
+++ 02.Functions/script.js
@@ -1,11 +1,15 @@
-var x = 1;
-var y = 2;
-var z = x + y;
+//adds two items together.
+//if the items are numbers, it will be a sum
+//if the items are strings, they will be stitched together
+function add(x, y) {
+    return x + y;
+}
+
+
+var z = add(1, 3);
 
 print("z is " + z);
 
-var firstName = "Bremerton";
-var lastName = "High";
-var fullName = firstName + " " + lastName;
+var fullName = add("Bremerton ", "High");
 
 print("fullName is " + fullName);
