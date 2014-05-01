2806ca0ed513e87af5267384c5601898151e9495
OpenShift guest <elixic@hotmail.com>
2014-05-01 02:17:44 -0400

    added a bit more to the lesson, logical practice and some comments.

--- 03.ControlFlow/script.js
+++ 03.ControlFlow/script.js
@@ -1,10 +1,45 @@
-var fight = function() {
+/**
+* This function is called by the ? link or the logic practice drop downs and uses the values
+* of the dropdowns to determine whether or not to display a True or False message
+* in the display area.
+* **/
+function logical() {
+    var display = document.getElementById('display');
+    var logic = document.getElementById('logic');
+    var leftSide = document.getElementById('leftside');
+    var rightSide = document.getElementById('rightside');
+    display.innerHTML = "<span>" + 
+        leftSide.value + " " + 
+        logic.value + " " + 
+        rightSide.value + "<span> ";
+    
+    if (logic.value === 'and') {
+        if (leftSide.value === 'true' && rightSide.value === 'true') {
+            display.innerHTML += "<span>is True</span>";
+        } else {
+            display.innerHTML += "<span>is False</span>";
+        }
+    } else {
+        if (leftSide.value === 'true' || rightSide.value === 'true') {
+            display.innerHTML += "<span>is True</span>";
+        } else {
+            display.innerHTML += "<span>is False</span>";
+        }
+    }
+};
+/**
+ * Uses the thefight form to look up values for the monsters and weapons drop down and
+ * display a message about whether or not you win or loose.
+ * **/
+function fight() {
     var form = document.getElementById('thefight');
     var results = document.getElementById('results');
     var weapon = form.weapon.value;
     var monster = form.monsters.value;
 
     results.innerHTML = "<p>You fought a " + monster + " with a " + weapon + "</p>"
+    
+    // change the logic here so that the vorpal blade always wins, and the zombie always dies
     
     if (weapon === 'wooden sword' && monster === 'Zombie') {
         results.innerHTML += "<h3>You Win!</h3>"
