6e6cd15b5be25fb22ae9be580909a6fd8d1f6dc6
Isaac Flemming <elixic@hotmail.com>
2014-06-05 00:15:16 -0400

    map file

---
+++ DungeonCrawler/scripts/map.js
@@ -0,0 +1,16 @@
+function Map(maps, doors, items) {
+    var map = {
+        maps: maps,
+        doors: doors,
+        items: items,
+        current: 0 // the current map (floor) to show.
+    };
+    
+    this.drawMap = function(element) {
+        
+    }
+    
+    this.updateCharacters = function(characters) {
+        
+    }
+}
