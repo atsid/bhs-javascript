7c807c9a85b509038f4c3779b7de2ca547f1769e
Isaac Flemming <elixic@hotmail.com>
2014-06-05 00:45:14 -0400

    player class

---
+++ DungeonCrawler/scripts/player.js
@@ -0,0 +1,35 @@
+function Player(name, position) {
+    var playerInfo = {
+        name: name,
+        position: position,
+        gold: 0,
+        experience: 0,
+        level: 1,
+        equipment: [],
+        weapon: undefined
+    };
+    
+    this.getPosition = function() {
+        return playerInfo.position;
+    }
+    
+    this.getName = function() {
+        return playerInfo.name;
+    }
+    
+    this.getGold() {
+        return playerInfo.gold;
+    }
+    
+    this.acceptGold = function(value) {
+        this.playerInfo.gold += value;
+    }
+    
+    this.acceptEquipment = function(value) {
+        this.playerInfo.equipment.push(value);
+    }
+    
+    this.combat = function(moster) {
+        // TODO : Resolve combat.
+    }
+}
