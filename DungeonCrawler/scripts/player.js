2f68f07b3a722472cc01a43c10822627cb7886b2
Isaac Flemming <isaacflemming@Isaacs-Mac-mini.local>
2014-06-04 22:05:48 -0700

    comments and another type in player.js

--- DungeonCrawler/scripts/player.js
+++ DungeonCrawler/scripts/player.js
@@ -9,31 +9,68 @@ function Player(name, position) {
         weapon: undefined
     };
 
+    /**
+     * Returns true, letting the game know this is a player character.
+     *
+     * @returns {boolean}
+     */
     this.isPlayerAgent = function() {
         return true;
     }
-    
+
+    /**
+     * returns the current position as x,y coordinates.
+     *
+     * @returns {*}
+     */
     this.getPosition = function() {
         return playerInfo.position;
     }
-    
+
+    /**
+     * returns the name of the player
+     *
+     * @returns {*}
+     */
     this.getName = function() {
         return playerInfo.name;
     }
-    
+
+    /**
+     * returns the gold for the player
+     *
+     * @returns {number}
+     */
     this.getGold = function() {
         return playerInfo.gold;
     }
-    
+
+    /**
+     * adds the value to the players gold.
+     *
+     * @param value
+     */
     this.acceptGold = function(value) {
-        this.playerInfo.gold += value;
+        if (value && value !== NaN) {
+            this.playerInfo.gold += value;
+        }
     }
-    
+
+    /**
+     * Adds the supplied item to the players equipment array.
+     *
+     * @param value
+     */
     this.acceptEquipment = function(value) {
         this.playerInfo.equipment.push(value);
     }
-    
-    this.combat = function(moster) {
+
+    /**
+     * Do combat with a monster.
+     *
+     * @param monster
+     */
+    this.combat = function(monster) {
         // TODO : Resolve combat.
     }
 }
